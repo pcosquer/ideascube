b1906bfef08ca8a39381d2cccb919b32c103b7fe
Mathieu Bridon <bochecha@daitauha.fr>
2016-05-28 16:35:38 +0200

    debian: Stop running the upgrade tasks in postinst
    Postinstall scripts should be as simple as possible, because they can't
    fail, under any circumstances.
    
    As a result, they are the wrong place to run things like database
    migrations and static files collection.
    
    Instead, let's run them when the application starts.
    
    Fixes #294
    Fixes #297

--- ideascube/wsgi.py
+++ ideascube/wsgi.py
@@ -11,5 +11,9 @@ import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ideascube.settings")
 os.environ.setdefault("STORAGE_ROOT", "/var/ideascube")
 
+from django.core.management import call_command
+call_command('migrate', '--noinput', '-v', '1')
+call_command('collectstatic', '--noinput', '-v', '1')
+
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
